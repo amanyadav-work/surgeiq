40f8e0f28cf652088716e41b9a48b62703f8b94e
Gautam-Sahil <193165968+Gautam-Sahil@users.noreply.github.com>
2025-11-29 01:46:02 +0530

    ui done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+
+import Footer from "@/components/ui/footer";
+import LenisScroll from "@/components/ui/lenis-scroll";
+import Navbar from "@/components/ui/navbar";
+import AboutOurApps from "@/sections/about-our-apps";
+import GetInTouch from "@/sections/get-in-touch";
+import HeroSection from "@/sections/hero-section";
+import OurLatestCreation from "@/sections/our-latest-creation";
+import OurTestimonials from "@/sections/our-testimonials";
+import SubscribeNewsletter from "@/sections/subscribe-newsletter";
+import TrustedCompanies from "@/sections/trusted-companies";
+
+export default function Page() {
+  return (
+    <>
+      <LenisScroll />
+      <Navbar />
+
+      <main className="px-6 md:px-16 lg:px-24 xl:px-32">
+        <HeroSection />
+        <OurLatestCreation />
+        <AboutOurApps />
+        <OurTestimonials />
+        <TrustedCompanies />
+        <GetInTouch />
+        <SubscribeNewsletter />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
